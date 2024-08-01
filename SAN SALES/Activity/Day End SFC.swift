5c41f90823316b8b7d39fdbfafc77e8cc2f33729
Mani <manisaneforce@gmail.com>
2024-08-01 18:27:51 +0530

    Add day End In Sfc

---
+++ SAN SALES/Activity/Day End SFC.swift
@@ -0,0 +1,29 @@
+//
+//  Day End SFC.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 01/08/24.
+//
+
+import UIKit
+
+class Day_End_SFC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
