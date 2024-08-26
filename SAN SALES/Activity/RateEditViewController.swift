3b4dcfcd51999aafb579074b9e9834cdc281efb3
Nagaprasathn <155469081+Nagaprasathn@users.noreply.github.com>
2024-08-26 18:33:23 +0530

    Secondary Order New Screen Completed

---
+++ SAN SALES/Activity/RateEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RateEditViewController.swift
+//  SAN SALES
+//
+//  Created by Naga Prasath on 24/08/24.
+//
+
+import UIKit
+
+class RateEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SAN SALES/Activity/SecondaryOrderNew.swift
@@ -0,0 +1,8 @@
+//
+//  SecondaryOrderNew.swift
+//  SAN SALES
+//
+//  Created by Naga Prasath on 20/08/24.
+//
+
+import Foundation
