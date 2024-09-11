7f5d67fa1d8a20c68317a07eb1d9926bb3ddbf09
Mani <manisaneforce@gmail.com>
2024-09-11 14:22:55 +0530

    Add Closing Sale Entry

---
+++ SAN SALES/Closing Entry Activity/Closing Sale Entry (DB).swift
@@ -0,0 +1,29 @@
+//
+//  Closing Sale Entry (DB).swift
+//  SAN SALES
+//
+//  Created by Anbu j on 11/09/24.
+//
+
+import UIKit
+
+class Closing_Sale_Entry__DB_: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
