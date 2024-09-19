235aa2539c4d662bf72a4b6759cd1d6eaaccebca
Mani <manisaneforce@gmail.com>
2024-09-19 16:49:45 +0530

    First Commit Target vs sales analysis

---
+++ SAN SALES/Reports/Target vs Sales Analysis.swift
@@ -0,0 +1,29 @@
+//
+//  Target vs Sales Analysis.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 19/09/24.
+//
+
+import UIKit
+
+class Target_vs_Sales_Analysis: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
