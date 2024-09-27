da98fd02f5d33c0430df50b8b348c7263db50557
Mani <manisaneforce@gmail.com>
2024-09-27 18:29:00 +0530

    AWS integration first commit

---
+++ SAN SALES/AWSS3Manager/AWSS3Manager.swift
@@ -0,0 +1,8 @@
+//
+//  AWSS3Manager.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 27/09/24.
+//
+
+import Foundation
---
+++ SAN SALES/AWSS3Manager/AWSS3_Download_Manager.swift
@@ -0,0 +1,8 @@
+//
+//  AWSS3_Download_Manager.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 27/09/24.
+//
+
+import Foundation
