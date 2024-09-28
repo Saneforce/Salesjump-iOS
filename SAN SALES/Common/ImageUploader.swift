5e09423d4453aeb99b04a5c557e40eb7cef6dd2d
Mani <manisaneforce@gmail.com>
2024-09-28 17:18:18 +0530

    Image Upload AWS

--- SAN SALES/Common/ImageUploader.swift
+++ SAN SALES/Common/ImageUploader.swift
@@ -13,32 +13,66 @@ class ImageUploader {
     func uploadImage(SFCode: String,image: UIImage,fileName: String)
     {
         
-         let imgData = image.jpegData(compressionQuality: 0.80)
-
-        AF.upload(multipartFormData: { multipartFormData in
-            multipartFormData.append(imgData!, withName:"imgfile", fileName:fileName, mimeType:"image/jpg")
-        }, to: APIClient.shared.BaseURL+APIClient.shared.DBURL1 + "imgupload&sf_code=" + SFCode)
-        .uploadProgress { progress in
-            print(progress)
+//         let imgData = image.jpegData(compressionQuality: 0.80)
+//
+//        AF.upload(multipartFormData: { multipartFormData in
+//            multipartFormData.append(imgData!, withName:"imgfile", fileName:fileName, mimeType:"image/jpg")
+//        }, to: APIClient.shared.BaseURL+APIClient.shared.DBURL1 + "imgupload&sf_code=" + SFCode)
+//        .uploadProgress { progress in
+//            print(progress)
+//        }
+//        .responseJSON { [weak self] response in
+//            print(response)
+//        }
+        
+        //New Code
+        AWSS3Manager.shared.uploadImage(image: image, progress: {[weak self] ( uploadProgress) in
+            
+            guard let strongSelf = self else { return }
+            
+        }) {[weak self] (uploadedFileUrl, error) in
+            
+            guard let strongSelf = self else { return }
+            if let finalPath = uploadedFileUrl as? String {
+                print(finalPath)
+            } else {
+                print("\(String(describing: error?.localizedDescription))")
+            }
         }
-        .responseJSON { [weak self] response in
-            print(response)
-        }
+        
     }
 }
 
 class ImageUploade {
     func uploadImage(SFCode: String, image: UIImage, fileName: String, completion: @escaping () -> Void) {
         let imgData = image.jpegData(compressionQuality: 0.25)
-        AF.upload(multipartFormData: { multipartFormData in
-            multipartFormData.append(imgData!, withName: "imgfile", fileName: fileName, mimeType: "image/jpg")
-        }, to: APIClient.shared.BaseURL+APIClient.shared.DBURL1 + "imgupload&sf_code=" + SFCode)
-        .uploadProgress { progress in
-            print(progress)
+//        AF.upload(multipartFormData: { multipartFormData in
+//            multipartFormData.append(imgData!, withName: "imgfile", fileName: fileName, mimeType: "image/jpg")
+//        }, to: APIClient.shared.BaseURL+APIClient.shared.DBURL1 + "imgupload&sf_code=" + SFCode)
+//        .uploadProgress { progress in
+//            print(progress)
+//        }
+//        .responseJSON { response in
+//            print(response)
+//            completion() // Call the completion handler when the response is received
+//        }
+        
+        //New Code
+        AWSS3Manager.shared.uploadImage(image: image, progress: {[weak self] ( uploadProgress) in
+            
+            guard let strongSelf = self else { return }
+            
+        }) {[weak self] (uploadedFileUrl, error) in
+            
+            guard let strongSelf = self else { return }
+            if let finalPath = uploadedFileUrl as? String {
+                print(finalPath)
+                completion()
+            } else {
+                print("\(String(describing: error?.localizedDescription))")
+                completion()
+            }
         }
-        .responseJSON { response in
-            print(response)
-            completion() // Call the completion handler when the response is received
-        }
+        
     }
 }
