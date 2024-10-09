abb0a23721bd32b4fdc186aae0bfdf9011e197bc
Mani <manisaneforce@gmail.com>
2024-10-09 18:26:33 +0530

    Com ss entry

---
+++ SAN SALES/Reports/Order Details.swift
@@ -0,0 +1,29 @@
+//
+//  Order Details.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 09/10/24.
+//
+
+import UIKit
+
+class Order_Details: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
