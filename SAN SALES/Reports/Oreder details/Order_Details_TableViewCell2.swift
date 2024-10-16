e495c851e439c650e18496b426e138313aa42889
Mani <manisaneforce@gmail.com>
2024-10-16 16:38:36 +0530

    Order Detials

---
+++ SAN SALES/Reports/Oreder details/Order_Details_TableViewCell2.swift
@@ -0,0 +1,23 @@
+//
+//  Order_Details_TableViewCell2.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 16/10/24.
+//
+
+import UIKit
+
+class Order_Details_TableViewCell2: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
