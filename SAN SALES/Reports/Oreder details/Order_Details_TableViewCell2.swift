59acd4243dd4b6d2321ab7d7fc0a6a3cb35f80e9
Mani <manisaneforce@gmail.com>
2024-10-16 16:38:53 +0530

    add detials

--- SAN SALES/Reports/Oreder details/Order_Details_TableViewCell2.swift
+++ SAN SALES/Reports/Oreder details/Order_Details_TableViewCell2.swift
@@ -7,17 +7,40 @@
 
 import UIKit
 
-class Order_Details_TableViewCell2: UITableViewCell {
+class Order_Details_TableViewCell2: UITableViewCell, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var insideTable2: UITableView!
+    var insideTable2Data: [String] = []
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        insideTable2.register(UITableViewCell.self, forCellReuseIdentifier: "InnerCell")
+        insideTable2.dataSource = self
+        insideTable2.delegate = self
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44.0
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return insideTable2Data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "InnerCell", for: indexPath)
+        cell.textLabel?.text = insideTable2Data[indexPath.row]
+        return cell
+    }
+    
+    func reloadData() {
+        print(insideTable2Data)
+        insideTable2.reloadData()
+    }
+}
 
+
+class Order_Details_TableViewCell3: UITableViewCell{
+    
+    
 }
