6120d829d11d53836e9377cdc83a2c36e7a61b17
Mani <manisaneforce@gmail.com>
2024-10-17 01:26:44 +0530

    Fixed Errors

--- SAN SALES/Reports/Oreder details/Order Details TableViewCell.swift
+++ SAN SALES/Reports/Oreder details/Order Details TableViewCell.swift
@@ -11,11 +11,36 @@ import UIKit
 class Order_Details_TableViewCell: UITableViewCell, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var insideTable1: UITableView!
-    var insideTable1Data: [String] = []
     
+    
+    let data = Order_Details()
+    var insideTable1Data: [Order_Details.OrderDetail] = []
+    
+    @IBOutlet weak var View_height: NSLayoutConstraint!
     @IBOutlet weak var Tbale2_height: NSLayoutConstraint!
     
-
+    @IBOutlet weak var Route_name: UILabel!
+    @IBOutlet weak var Stockets_Name: UILabel!
+    
+    @IBOutlet weak var Store_Name_with_order_No: UILabel!
+    
+    @IBOutlet weak var Addres: UILabel!
+    
+    @IBOutlet weak var Volumes: UILabel!
+    
+    @IBOutlet weak var Phone: UILabel!
+    
+    
+    @IBOutlet weak var Netamt: UILabel!
+    
+    @IBOutlet weak var Remark: UILabel!
+    
+    
+    
+    @IBOutlet weak var Product_Name: UILabel!
+    @IBOutlet weak var Qty: UILabel!
+    @IBOutlet weak var Free: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -23,19 +48,31 @@ class Order_Details_TableViewCell: UITableViewCell, UITableViewDataSource, UITab
         insideTable1.register(UITableViewCell.self, forCellReuseIdentifier: "InnerCell")
         insideTable1.dataSource = self
         insideTable1.delegate = self
-        Tbale2_height.constant = 50
+        print(Tbale2_height.constant)
+    
+       // View_height.constant = 0
+            Tbale2_height.constant = 150
+        
+        print(Tbale2_height.constant)
         
     }
     
     // DataSource for the outer table (insideTable1)
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(insideTable1Data)
             return insideTable1Data.count
     }
 
     // Cell creation for both tables
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCell(withIdentifier: "InnerCell", for: indexPath)
-            cell.textLabel?.text = insideTable1Data[indexPath.row]
+        let flg = insideTable1Data[indexPath.row].Routeflg
+        if flg == "1"{
+            View_height.constant = 68
+        }else{
+            View_height.constant = 0
+        }
+        
             return cell
        
     }
