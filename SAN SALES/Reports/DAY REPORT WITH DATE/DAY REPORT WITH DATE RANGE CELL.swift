a709aa0593ac2bb914bafb73e4936bf17386bc98
Mani <manisaneforce@gmail.com>
2024-10-22 15:34:28 +0530

    Add Day Report

---
+++ SAN SALES/Reports/DAY REPORT WITH DATE/DAY REPORT WITH DATE RANGE CELL.swift
@@ -0,0 +1,23 @@
+//
+//  DAY REPORT WITH DATE RANGE CELL.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 22/10/24.
+//
+
+import UIKit
+
+class DAY_REPORT_WITH_DATE_RANGE_CELL: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
