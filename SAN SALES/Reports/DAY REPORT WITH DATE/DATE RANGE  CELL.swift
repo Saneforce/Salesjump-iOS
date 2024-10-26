90f9c96aade48fa92d8e6c82beb22e0381bf365d
Mani <manisaneforce@gmail.com>
2024-10-26 17:52:16 +0530

    Add Range Cell

---
+++ SAN SALES/Reports/DAY REPORT WITH DATE/DATE RANGE  CELL.swift
@@ -0,0 +1,23 @@
+//
+//  DATE RANGE  CELL.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 26/10/24.
+//
+
+import UIKit
+
+class DATE_RANGE__CELL: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SAN SALES/Reports/DAY REPORT WITH DATE/DAY REPORT WITH DATE RANGE DETAILSViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DAY REPORT WITH DATE RANGE DETAILSViewController.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 26/10/24.
+//
+
+import UIKit
+
+class DAY_REPORT_WITH_DATE_RANGE_DETAILSViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
