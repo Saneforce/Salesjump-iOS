a790c165d748c7c00e054f0f4c9591cb6843934f
Mani <manisaneforce@gmail.com>
2024-11-05 17:03:32 +0530

    Add Distributor Cell

---
+++ SAN SALES/Reports/DISTRIBUTOR ORDER DETAILS/Distributor Order Details cell.swift
@@ -0,0 +1,29 @@
+//
+//  Distributor Order Details cell.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 05/11/24.
+//
+
+import UIKit
+
+class Distributor_Order_Details_cell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SAN SALES/Reports/DISTRIBUTOR ORDER DETAILS/Distributor TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  Distributor TableViewCell.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 05/11/24.
+//
+
+import UIKit
+
+class Distributor_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
