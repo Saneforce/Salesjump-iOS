4b39effa1e974e65367f98afe9a507d7467adc75
Mani <manisaneforce@gmail.com>
2024-12-06 18:37:03 +0530

    Secondary Order Details UI Completed

--- SAN SALES/Reports/Secondary order details/Secondary_Order_Details_CustomTableViewCell.swift
+++ SAN SALES/Reports/Secondary order details/Secondary_Order_Details_CustomTableViewCell.swift
@@ -8,30 +8,11 @@
 import Foundation
 import UIKit
 
-class Secondary_Order_Details_Customcell: UITableViewCell {
-    
-    
+class Secondary_Order_Details_Customcell: UITableViewCell{
     @IBOutlet weak var Card_View: UIView!
     @IBOutlet weak var Orderid: UILabel!
     @IBOutlet weak var amt: UILabel!
     @IBOutlet weak var Retiler_Nmae: UILabel!
     @IBOutlet weak var Retiler_Address: UILabel!
     
-    @IBOutlet weak var dynamicLabel: UILabel!
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        Orderid.numberOfLines = 1
-        amt.numberOfLines = 1
-        Retiler_Nmae.numberOfLines = 1
-        Retiler_Address.numberOfLines = 0 // Allow unlimited lines
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        Orderid.preferredMaxLayoutWidth =  Orderid.frame.width
-        amt.preferredMaxLayoutWidth = amt.frame.width
-        Retiler_Nmae.preferredMaxLayoutWidth = Retiler_Nmae.frame.width
-        Retiler_Address.preferredMaxLayoutWidth = Retiler_Address.frame.width
-    }
 }
