81f4a989e805ae4cb06d9cddaba8ee3aadedfa1b
Mani <manisaneforce@gmail.com>
2024-12-23 17:46:11 +0530

    Add Custom Fields in Add New Retailer Screan

---
+++ SAN SALES/Activity/AddNewCustomer_New.swift
@@ -0,0 +1,29 @@
+//
+//  AddNewCustomer_New.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 23/12/24.
+//
+
+import UIKit
+
+class AddNewCustomer_New: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
