4564a58e18345689f325917d9d9b505bb8cadb05
Mani <manisaneforce@gmail.com>
2025-01-02 15:58:05 +0530

    Edit Customer New

---
+++ SAN SALES/Activity/Edit Customer.swift
@@ -0,0 +1,29 @@
+//
+//  Edit Customer.swift
+//  SAN SALES
+//
+//  Created by Anbu j on 02/01/25.
+//
+
+import UIKit
+
+class Edit_Customer: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
